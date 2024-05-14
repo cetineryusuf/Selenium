0b6f3f9677b48b36a2a78d55035bd512358dd2dd
Yusuf <164163159+cetineryusuf@users.noreply.github.com>
2024-05-15 02:03:36 +0300

    Add files via upload

---
+++ Test_odev/constant/globalconstant.py
@@ -0,0 +1,15 @@
+url="https://www.saucedemo.com/"
+username_id="user-name"
+password_İD="password"
+logginButton_id="login-button"
+errmassage_xpath="//div[@id='login_button_container']/div/form/div[3]/h3"
+excelUzantısı="data/pair4unlogin.xlsx"
+
+
+errmassage_xpathbos="//div[@class='error-message-container error']"
+baslık_path="//div[@class='app_logo']"
+
+excelBasarılı="data/Kitap1.xlsx"
+
+excel3="data/abcd.xlsx"
+errorLocked="//div[@class='error-message-container error']"
